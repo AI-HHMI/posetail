9516d3d023ca1ff92e1f5412e261a6e4fc17d77b
Lili Karashchuk <krchtchk@gmail.com>
2026-06-26 12:24:31 -0400

    commit best config

--- big_bsub_pretraining.sh
+++ big_bsub_pretraining.sh
@@ -13,5 +13,5 @@ d=$(date +d%dh%H%M)
 
 
 bsub -J en6-$d -e ~/logs/posetail/en6-$d.err -o ~/logs/posetail/en6-$d.out \
-    -n 48 -q gpu_h200 -R "span[hosts=1]" -gpu "num=4:aff=yes" -W 72:00 \
+    -n 48 -q gpu_h100 -R "span[hosts=1]" -gpu "num=4:aff=yes" -W 72:00 \
     /bin/bash train_script.sh configs/config_encoder_pretrain.toml
