650c5fe1ad1bbb34bc94ac3b37abb6a9b45a3afe
Lili Karashchuk <krchtchk@gmail.com>
2026-07-25 00:59:48 +0000

    restore the nccl env overrides in the cluster scripts

--- cluster/train_script.sh
+++ cluster/train_script.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+export NCCL_NET_GDR_LEVEL=0
+export NCCL_BUFFSIZE=8388608
+export NCCL_P2P_DISABLE=0
+export NCCL_SHM_DISABLE=0
+
 source cluster/setup_env_vars.sh
 
 module load cuda/12.8
